676fc54453a011f30445a8fc650e41da112ff5d2
Jason Ho <jason.ho@obf.ateneo.edu>
2022-06-23 14:40:29 +0800

    Add validators

--- python/bank_account.py
+++ python/bank_account.py
@@ -4,14 +4,20 @@ class BankAccount:
         self.balance = balance
 
     def deposit(self, amount):
-        self.balance += amount
-        print('You just deposited ${}'.format(amount))
-        self.check_balance()
+        if amount > 0:
+            self.balance += amount
+            print('You just deposited ${}'.format(amount))
+            self.check_balance()
+        else:
+            print('Invalid deposit amount')
 
     def withdraw(self, amount):
-        self.balance -= amount
-        print('You just withdrew ${}'.format(amount))
-        self.check_balance()
+        if self.balance >= amount:
+            self.balance -= amount
+            print('You just withdrew ${}'.format(amount))
+            self.check_balance()
+        else:
+            print('Insufficient balance')
 
     def check_balance(self):
         print('Your current balance is ${}'.format(self.balance))
@@ -24,3 +30,7 @@ jason.check_balance()
 jason.deposit(200)
 
 jason.withdraw(300)
+
+jason.deposit(-20)
+
+jason.withdraw(905)
