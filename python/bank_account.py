7a9e4281540494e70ee627e41bed881ff3cf2c11
Jason Ho <jason.ho@obf.ateneo.edu>
2022-06-23 15:02:14 +0800

    Rename puts function

--- python/bank_account.py
+++ python/bank_account.py
@@ -1,4 +1,4 @@
-def puts(str):
+def print_with_line_break(str):
     print('{}\n'.format(str))
 
 
@@ -10,24 +10,24 @@ class BankAccount:
     def deposit(self, amount):
         if amount > 0:
             self.balance += amount
-            puts(f'You just deposited ${amount}')
+            print_with_line_break(f'You just deposited ${amount}')
             self.get_balance()
         else:
-            puts('Invalid deposit amount')
+            print_with_line_break('Invalid deposit amount')
 
     def withdraw(self, amount):
         if self.balance >= amount:
             self.balance -= amount
-            puts(f'You just withdrew ${amount}')
+            print_with_line_break(f'You just withdrew ${amount}')
             self.get_balance()
         else:
-            puts('Insufficient balance')
+            print_with_line_break('Insufficient balance')
 
     def get_balance(self):
-        puts(f'Your current balance is ${self.balance}')
+        print_with_line_break(f'Your current balance is ${self.balance}')
 
     def get_account_name(self):
-        puts(f'Your account name is {self.account_name}')
+        print_with_line_break(f'Your account name is {self.account_name}')
 
 
 jason = BankAccount('Jason Ho', 1000)
